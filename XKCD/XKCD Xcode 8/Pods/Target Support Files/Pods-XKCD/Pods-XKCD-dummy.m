e99c1df5714d275527d71b2b117a1e5ffbb972ae
Micha Mazaheri <micha@mazaheri.me>
2017-09-22 15:20:30 +0200

    Add all Pods

---
+++ XKCD/XKCD Xcode 8/Pods/Target Support Files/Pods-XKCD/Pods-XKCD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XKCD : NSObject
+@end
+@implementation PodsDummy_Pods_XKCD
+@end
---
+++ XKCD/XKCD Xcode 8/Pods/Target Support Files/Pods-XKCDTests/Pods-XKCDTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XKCDTests : NSObject
+@end
+@implementation PodsDummy_Pods_XKCDTests
+@end
